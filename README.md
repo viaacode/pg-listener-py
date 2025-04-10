5bff126d3882f0facaabb63d31932d289b0263e2
Maarten <maartendeschrijver@gmail.com>
2025-04-10 16:58:29 +0200

    MVP version

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # pg-listener-py
+
+Python service that publishes Postgres LISTEN/NOTIFY events to Pulsar.
+
+See:
+- [PostgreSQL NOTIFY](https://www.postgresql.org/docs/current/sql-notify.html)
+- [PostgreSQL LISTEN](https://www.postgresql.org/docs/current/sql-listen.html)
+
+## Prerequisites
+
+* Python 3.10+
+* Access to the meemoo PyPI (VPN required)
+
+## Development and testing
+
+1. Clone this repository and change into the new dir:
+
+```bash
+git clone git@github.com:viaacode/pg-listener-py.git
+cd pg-listener-py
+```
+
+2. Init and activate a  virtual env:
+
+```bash
+python3 -m venv .venv
+source .venv/bin/activate
+```
+
+3. Install the library and the dev/test dependencies:
+
+```bash
+(.venv) python -m pip install . \
+    --extra-index-url http://do-prd-mvn-01.do.viaa.be:8081/repository/pypi-all/simple \
+    --trusted-host do-prd-mvn-01.do.viaa.be
+```
+
+## Usage
+
+1. Fill in and export the environment variables in `.env`:
+
+```bash
+(.venv) export $(grep -v '^#' .env | xargs)
+```
+
+2. Run with
+
+```bash
+(.venv) python main.py
+```
---
+++ main.py
@@ -0,0 +1,56 @@
+# Std
+import os
+import asyncio
+# 3d Party
+import psycopg
+# meemoo
+# internal
+from cloudevents.events import (
+    CEMessageMode,
+    Event,
+    EventAttributes,
+    EventOutcome,
+    PulsarBinding,
+)
+from viaa.configuration import ConfigParser
+from viaa.observability import logging
+
+APP_NAME = "pg-listener-py"
+
+# Init the config and the logger
+config_parser = ConfigParser()
+config = config_parser.app_cfg
+log = logging.get_logger(__name__, config=config_parser)
+
+
+def run_main():
+    log.info(f'Starting listener on channel {config["db"]["channel"]}')
+    conn = psycopg.connect(
+        host=config["db"]["host"],
+        dbname=config["db"]["name"],
+        user=config["db"]["user"],
+        password=config["db"]["passwd"]
+    )
+
+    cursor = conn.cursor()
+    cursor.execute(f'LISTEN {config["db"]["channel"]};')
+    conn.commit()
+
+    def handle_notify():
+        try:
+            for notify in conn.notifies(stop_after=0):
+                print(notify.payload)
+        except:
+            log.error('Error occurred')
+            raise
+
+    loop = asyncio.get_event_loop()
+    loop.add_reader(conn, handle_notify)
+    loop.run_forever()
+
+if __name__ == "__main__":
+    try:
+        log.info(f'Starting')
+        run_main()
+    finally:
+        log.info('Exiting')
