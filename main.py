ae8cf3a3ac14617a0d46f8c00d36e7aa4d895676
Jan <170526384+jaameemoo@users.noreply.github.com>
2025-04-16 17:15:57 +0200

    Send cloudevents

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 # std
 import argparse
 import asyncio
+import json
+import logging as std_logging
+from retry import retry
 
 # 3rd party
 from psycopg import connect as pg_connect, sql
+import pulsar
+from pulsar import Client as PulsarClient
 
 # meemoo internal
 from cloudevents.events import (
     CEMessageMode,
     Event,
     EventAttributes,
-    EventOutcome,
     PulsarBinding,
 )
 from viaa.configuration import ConfigParser
@@ -21,10 +25,81 @@ config_parser = ConfigParser()
 config = config_parser.app_cfg
 log = logging.get_logger(__name__, config=config_parser)
 
-APP_NAME = config["self"]
+APP_NAME = config["self"]["name"]
+
+
+@retry(pulsar.ConnectError, tries=10, delay=1, backoff=2)
+def get_pulsar_client():
+    """Return a Pulsar client."""
+    url = "pulsar://{host}:{port}/"
+    url = url.format(**config["pulsar"])
+    log.info(f"Connecting to Pulsar using service URL {url}")
+
+    # pass a logger to avoid unformatted logging to stdout
+    pulsar_logger = std_logging.getLogger("pulsar")
+    pulsar_logger.setLevel(std_logging.CRITICAL)
+    try:
+        client = PulsarClient(service_url=url, logger=pulsar_logger)
+        return client
+    except Exception as e:
+        error_message = f"failed to create Pulsar client: {e}"
+        log.error(error_message, error=e)
+        raise Exception(error_message)
+
+
+@retry(pulsar.ConnectError, tries=10, delay=1, backoff=2)
+def get_pulsar_producer(client):
+    """Return a Pulsar producer."""
+    topic = "persistent://public/{namespace}/{topic}".format(
+        namespace=config["pulsar"]["namespace"],
+        topic=config["pulsar"]["topic"],
+    )
+    log.info(
+        "Creating producer {name} on topic {topic}".format(
+            name=APP_NAME,
+            topic=topic,
+        )
+    )
+    producer = client.create_producer(
+        topic=topic,
+        producer_name=APP_NAME,
+    )
+    return producer
+
+
+def send_event(producer, data):
+    data = json.loads(data)
+    if data["correlation_id"]:
+        correlation_id = data["correlation_id"]
+        del data["correlation_id"]
+        attributes = EventAttributes(
+            type=config["pulsar"]["topic"],
+            source=APP_NAME,
+            subject=data["essence_name"],
+            correlation_id=correlation_id,
+        )
+    else:
+        attributes = EventAttributes(
+            type=config["pulsar"]["topic"],
+            source=APP_NAME,
+            subject=data["essence_name"],
+        )
+
+    event = Event(attributes, data)
+    create_msg = PulsarBinding.to_protocol(event, CEMessageMode.STRUCTURED.value)
+
+    message_id = producer.send(
+        create_msg.data,
+        properties=create_msg.attributes,
+        event_timestamp=event.get_event_time_as_int(),
+    )
+    log.info(f"sent a Pulsar event with ID {message_id}")
 
 
 def main(args: argparse.Namespace):
+    client = get_pulsar_client()
+    producer = get_pulsar_producer(client)
+
     pg_channel_name = args.channel_name or config["db"]["channel"]
 
     log.info(f"Starting listener on channel {pg_channel_name}")
@@ -46,7 +121,9 @@ def main(args: argparse.Namespace):
     def handle_notify():
         try:
             for notify in conn.notifies(stop_after=0):
-                print(notify.payload)
+                log.debug(f"Got a Postgres notification with payload {notify.payload}")
+                send_event(producer, data=notify.payload)
+
         except Exception as e:
             log.error(f"Error occurred while handling Postgres notification: {e}")
             raise
